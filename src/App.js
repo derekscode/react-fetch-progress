2387c1df20efee4c9359f7d40d02d5d015631d0a
Derek Heeren <derek.heeren@zoom.us>
2022-09-22 16:52:43 -0700

    add

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+import LinearProgress from "@mui/material/LinearProgress";
 
 function App() {
-    const [percentage, setPercentage] = useState(0);
-    const [progress,   setProgress] = useState(null);
+  const [percentage, setPercentage] = useState(0);
+  const [progress, setProgress] = useState(null);
 
-    const download = () => {
-        const documentStyles = document.documentElement.style;
-        let progress = 0;
-    
-        setProgress('in-progress');
-    
-        axios({
-            url: 'https://www.placecage.com/3499/3499',
-            onDownloadProgress(progressEvent) {
-                progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+  const download = () => {
+    const documentStyles = document.documentElement.style;
+    let progress = 0;
 
-                setPercentage(progress);
+    setProgress("in-progress");
 
-                documentStyles.setProperty('--progress', `${progress}%`);
-            }
-        }).then(response => {
-            setProgress('finished');
-        });
-    };
+    axios({
+      url: "https://www.placecage.com/3499/3499",
+      onDownloadProgress(progressEvent) {
+        progress = Math.round(
+          (progressEvent.loaded / progressEvent.total) * 100
+        );
 
-    return (
-        <div className={`progress-button ${progress}`}>
-            <span className="loading-text">Loading</span>
-                <button className="download-button" onClick={download}>
-                    <span className="button-text">{progress === 'finished' ? '🎉 Done' : 'Download'}</span>
-                </button>
-            <span className="percentage">{percentage}%</span>
-        </div>
-    );
+        setPercentage(progress);
+
+        documentStyles.setProperty("--progress", `${progress}%`);
+      },
+    }).then((response) => {
+      setProgress("finished");
+    });
+  };
+
+  return (
+    <LinearProgress variant="determinate" value={progress} />
+
+    // <div className={`progress-button ${progress}`}>
+    //     <span className="loading-text">Loading</span>
+    //         <button className="download-button" onClick={download}>
+    //             <span className="button-text">{progress === 'finished' ? '🎉 Done' : 'Download'}</span>
+    //         </button>
+    //     <span className="percentage">{percentage}%</span>
+    // </div>
+  );
 }
 
 export default App;
