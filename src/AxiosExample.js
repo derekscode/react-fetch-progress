92f3d8d7c1a35687dae09ae086b674abfcc0d6e7
Derek Heeren <derek.heeren@zoom.us>
2022-09-27 17:01:21 -0700

    add

--- src/AxiosExample.js
+++ src/AxiosExample.js
@@ -16,7 +16,7 @@ const CSVDownloadType = {
 
 export const AxiosExample = () => {
   const downloadCsvFile = (url) => {
-    console.log("downloading file...");
+    // console.log("downloading file...");
     Toast.info({
       content: "Downloading file",
       duration: 0,
@@ -27,19 +27,16 @@ export const AxiosExample = () => {
     const start = Date.now();
 
     axios({
-      // 1  Event Details Page
-      // 9  Registration Report
       url: `http://0.0.0.0:9001/api/v1/e/v/panel/control/download/csv/${tenThousand}?csvType=${CSVDownloadType.REGISTRATION_REPORT}`, //your url
       method: "GET",
       responseType: "blob", // important
     }).then((response) => {
       // create file link in browser's memory
       const href = URL.createObjectURL(response.data);
-
-      // create "a" HTLM element with href to file & click
+      // create "a" HTML element with href to file & click
       const link = document.createElement("a");
       link.href = href;
-      // link.setAttribute("download", "file.pdf"); //or any other extension
+      // link.setAttribute("download", "file.pdf"); // or any other extension
       document.body.appendChild(link);
       link.click();
 
@@ -47,7 +44,7 @@ export const AxiosExample = () => {
       document.body.removeChild(link);
       URL.revokeObjectURL(href);
 
-      console.log("download finished!");
+      // console.log("download finished!");
       Toast.destroy("unique key");
       const end = Date.now();
       console.log(`Time elapsed:  ${(end - start) / 1000} seconds`);
